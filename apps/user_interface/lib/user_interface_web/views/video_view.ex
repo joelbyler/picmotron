85d0a65999ff202742ebeafbb6b914c09723031f
Joel Byler <jbyler@covermymeds.com>
2017-12-28 21:37:45 -0500

    Cleanup UI

--- apps/user_interface/lib/user_interface_web/views/video_view.ex
+++ apps/user_interface/lib/user_interface_web/views/video_view.ex
@@ -1,3 +1,10 @@
 defmodule UserInterfaceWeb.VideoView do
   use UserInterfaceWeb, :view
+
+  def video_image_source do
+    case Mix.env do
+      :dev -> "/images/placeholder.jpg"
+      _ -> "/video.mjpg"
+    end
+  end
 end
