6da363e772342720e7c4825e1e9a77a12835b312
Joel Byler <jbyler@covermymeds.com>
2018-01-10 09:51:37 -0500

    fix image rotation and remove extra comments

--- apps/camera_controls/lib/camera_controls.ex
+++ apps/camera_controls/lib/camera_controls.ex
@@ -1,9 +1,26 @@
 defmodule CameraControls do
+
+  def capture_photo() do
+    camera.set_rotation(90)
+    camera.set_size(1028, 0)
+
+    File.mkdir(image_location())
+    File.write!(
+      Path.join(image_location(),
+      "#{next_image_number()}.jpg"),
+      camera.next_frame
+    )
+  end
+
   def images do
     File.ls(image_location())
      |> parse_ls
      |> Enum.sort(&(image_number(&1) >= image_number(&2)))
      |> Enum.map(&path_for_image(&1))
+  end
+
+  defp camera do
+    CameraControls.Camera.adapter
   end
 
   defp next_image_number do
@@ -35,11 +52,5 @@ defmodule CameraControls do
 
   defp image_location do
     Application.get_env(:camera, :image_location) || "/root/images"
-  end
-
-  def capture_photo() do
-    File.mkdir(image_location())
-    File.write!(Path.join(image_location(), "#{next_image_number()}.jpg"), CameraControls.Camera.adapter.next_frame)
-
   end
 end
--- apps/firmware/lib/firmware/application.ex
+++ apps/firmware/lib/firmware/application.ex
@@ -1,12 +1,9 @@
 defmodule Firmware.Application do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    # Define workers and child supervisors to be supervised
     children = [
       worker(CameraControls.Camera.camera(), []),
       worker(CameraControls.Timelapse, [])
@@ -14,23 +11,20 @@ defmodule Firmware.Application do
 
     setup_network()
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Firmware.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
   defp setup_network do
+    # Give OS some time to initialize
     :timer.sleep(5000)
+
     System.cmd("sysctl", ["-w", "net.ipv4.ip_forward=1"]) |> print_cmd_result
-    #
-    # # System.cmd("ip", ["route", "add", "default", "via", default_gateway]) |> print_cmd_result
-    #
+
     System.cmd("ip", ["link", "set", "wlan0", "up"]) |> print_cmd_result
     System.cmd("ip", ["addr", "add", "192.168.24.1/24", "dev", "wlan0"]) |> print_cmd_result
-    #
+
     System.cmd("dnsmasq", ["--dhcp-lease", "/root/dnsmasq.lease"]) |> print_cmd_result
-    #
     System.cmd("hostapd", ["-B", "-d", "/etc/hostapd/hostapd.conf"]) |> print_cmd_result
   end
 
